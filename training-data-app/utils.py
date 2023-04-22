d688a47e8ea95ccc643e3758867ea400335808a0
Youssef El Ouard <youselouard@gmail.com>
2023-04-23 00:34:47 +0200

    Updated utils to better handle credentials

--- training-data-app/utils.py
+++ training-data-app/utils.py
@@ -1,6 +1,7 @@
 from google.cloud import storage
+from google.oauth2 import service_account
 import os
-from .params import *
+from params import *
 
 
 def save_playlist_data(playlist: dict) -> list:
@@ -23,7 +24,10 @@ def push_to_cloud():
     """
     Pushes training data to a cloud bucket
     """
-    client = storage.Client()
+    credentials = service_account.Credentials.from_service_account_file(
+        PATH_TO_CREDENTIALS
+    )
+    client = storage.Client(credentials=credentials)
     bucket = client.bucket(GCP_BUCKET_NAME)
 
     for file in os.listdir("training-data"):
